c3a8c699deee83549f13b94c95dc6bfb32e8a269
Omp <kantee2540@hotmail.com>
2019-12-31 02:00:38 +0700

    Add user backend

---
+++ collectUserData.php
@@ -0,0 +1,37 @@
+<?php
+include("connectDB.php");
+
+$firstName = $_POST["first_name"];
+$lastName = $_POST["last_name"];
+$nickName = $_POST["nick_name"];
+$email = $_POST["email"];
+$gender = $_POST["gender"];
+$birthday = $_POST["birthday"];
+$uid = $_POST["uid"];
+$option = $_POST["option"];
+
+// 0 = Insert
+// 1 = Update
+if ($option == '0'){
+    $sql = "INSERT INTO `user` (`user_id`, `first_name`, `last_name`, `nickname`, `email`, `gender`, `birthday`, `uid`) VALUES (NULL, '$firstName', '$lastName', '$nickName', '$email', '$gender', '$birthday', '$uid ');";
+}else if ($option == '1'){
+    $sql = "UPDATE `user` SET first_name = '$firstName', last_name = '$lastName', nickname = '$nickName', gender = '$gender', birthday = '$birthday' WHERE uid = '$uid';";
+}else{
+    $sql = "INSERT INTO `user` (`user_id`, `first_name`, `last_name`, `nickname`, `email`, `gender`, `birthday`, `uid`) VALUES (NULL, '', '', '', '', '', '', '' ');";
+}
+
+if($connectDB->query($sql) === true){
+    $response['error'] = false;
+    $response['message'] = "Insert data successfully";
+}
+else{
+    $response['error'] = true;
+    $response['message'] = "Error cannot insert data!";
+}
+
+echo json_encode($response);
+
+
+mysqli_close($connectDB);
+
+?>
---
+++ getUser.php
@@ -0,0 +1,27 @@
+<?php
+include("connectDB.php");
+$uid = $_POST["uid"];
+
+$sql = "SELECT * FROM user WHERE uid = '$uid'";
+
+if($result = mysqli_query($connectDB, $sql)){
+
+    $resultArray = array();
+    $tempArray = array();
+
+    while($row = $result->fetch_object()){
+        $tempArray = $row;
+        array_push($resultArray, $tempArray);
+    }
+
+        echo json_encode($resultArray);
+}
+else{
+    echo "ERROR!! Check connection or sql syntax";
+}
+
+
+mysqli_close($connectDB);
+
+
+?>
