1437bef146e280315235c774851a6964ef080f78
Omp <kantee2540@hotmail.com>
2020-03-21 01:45:56 +0700

    Add Limit to cosmetic desk

--- getCosmeticDeskList.php
+++ getCosmeticDeskList.php
@@ -5,8 +5,24 @@ $user_id = $_POST["user_id"];
 
 if(isset($_POST["product_id"])){
     $product_id = $_POST["product_id"];
-    $sql = "SELECT * FROM cosmetic_desk c, product p, product_brand b WHERE c.product_id = p.product_id AND p.brand_id = b.brand_id AND c.user_id = '$user_id' AND c.product_id = '$product_id'";
-}else{
+    $sql = "SELECT * FROM cosmetic_desk c, product p, product_brand b 
+    WHERE c.product_id = p.product_id 
+    AND p.brand_id = b.brand_id 
+    AND c.user_id = '$user_id' 
+    AND c.product_id = '$product_id'";
+
+}else if(isset($_POST["limit"])){
+    $limit = $_POST["limit"];
+    $sql = "SELECT * FROM cosmetic_desk c, product p, product_brand b 
+    WHERE c.product_id = p.product_id 
+    AND p.brand_id = b.brand_id
+    AND c.user_id = '$user_id'
+    ORDER BY c.desk_id DESC
+    LIMIT $limit";
+
+}
+
+else{
     $sql = "SELECT * FROM cosmetic_desk c, product p, product_brand b WHERE c.product_id = p.product_id AND p.brand_id = b.brand_id AND c.user_id = '$user_id' ORDER BY c.desk_id DESC";
 }
 
